8524dc7333d0d9fccae3fdbd8b696ffc85780e70
alialkhalil <alialkhalil.717@gmail.com>
2026-07-28 13:13:54 -0400

    fix app.jsx

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Clinic Management</title>
     @viteReactRefresh
-    @vite(['resources/js/app.jsx', 'resources/css/app.css'])
+    @vite(['resources/js/main.jsx', 'resources/css/app.css'])
 </head>
 <body>
     <div id="root"></div>
